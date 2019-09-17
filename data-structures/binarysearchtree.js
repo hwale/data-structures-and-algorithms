6fad506eac28f7ae394f24e9a750e87193f19e95
Charlie <sketchblock@gmail.com>
2019-09-16 20:13:21 -0700

    tree traversal breadth first

--- data-structures/binarysearchtree.js
+++ data-structures/binarysearchtree.js
@@ -19,21 +19,65 @@ class BinarySearchTree {
         }
         let current = this.root;
         while (true) {
+            if (val === current.val) return undefined;
             if (val < current.val) {
                 if (!current.left) {
                     current.left = newNode;
                     return this;
-                } else {
-                    current = current.left;
                 }
-            } else if (val > current.val) {
+                current = current.left;
+            } else {
                 if (!current.right) {
                     current.right = newNode;
                     return this;
-                } else {
-                    current = current.right;
                 }
+                current = current.right;
             }
         }
+    }
+
+    find(val) {
+        if (!this.root) return false;
+
+        let currentNode = this.root;
+        while (true) {
+            if (val === currentNode.val) return currentNode;
+            if (val < currentNode.val) {
+                if (!currentNode.left) return false;
+                currentNode = currentNode.left;
+            } else {
+                if (!currentNode.right) return false;
+                currentNode = currentNode.right;
+            }
+        }
+    }
+
+    contains(val) {
+        if (!this.root) return false;
+
+        let currentNode = this.root;
+        while (true) {
+            if (val === currentNode.val) return true;
+            if (val < currentNode.val) {
+                if (!currentNode.left) return false;
+                currentNode = currentNode.left;
+            } else {
+                if (!currentNode.right) return false;
+                currentNode = currentNode.right;
+            }
+        }
+    }
+
+    breadthFirstSearch() {
+        const queue = [];
+        const visited = [];
+        if (this.root) queue.push(this.root);
+        while (queue.length) {
+            const dequeued = queue.shift();
+            visited.push(dequeued);            
+            if (dequeued.left) queue.push(dequeued.left);
+            if (dequeued.right) queue.push(dequeued.right);
+        }
+        return visited;
     }
 }
